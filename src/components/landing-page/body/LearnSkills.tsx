a5ff67c99f4cb30c45c78613439e30784fc0cc90
Alex Strugacz <alexstrugacz@icloud.com>
2022-12-26 17:27:52 -0600

    update

--- src/components/landing-page/body/LearnSkills.tsx
+++ src/components/landing-page/body/LearnSkills.tsx
@@ -6,9 +6,9 @@ const LearnSkills: React.FC<{}> = () => {
         <div className=" bg-zinc-200 shadow-md">
             <div className="flex justify-center">
                 <img className="m-12" src={learnskills}></img>
-                <div className="m-12">
-                    <h3 className="font-Grotesk text-3xl">Learn new coding skills</h3>
-                    <p className="flex">Hello</p>
+                <div className=" m-12">
+                    <h3 className="w-1/2 font-Grotesk font-bold text-3xl">Learn new <a className="text-teal-600">coding skills</a></h3>
+                    <p className="mt-12 w-1/2 center">For the first 20 hours of the internship, learn necessary skills for your web or iOS development project through a Udemy course. Course costs are covered by Hersey Developers.</p>
                 </div>
 
             </div>
