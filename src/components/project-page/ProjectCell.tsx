9e6254804022fc744337518d8ea797dc83cc981b
nexus1010 <bot.developer3@gmail.com>
2022-12-30 18:46:26 -0600

    Added padding fix

--- src/components/project-page/ProjectCell.tsx
+++ src/components/project-page/ProjectCell.tsx
@@ -9,9 +9,9 @@ const ProjectCell: React.FC<{
     status: string;
 }> = (project) => {
     return <a href={project.link}>
-        <div className='bg-gradient-to-b from-neutral-600 to-neutral-900 rounded-2xl'>
+        <div className='bg-gradient-to-b from-neutral-500 to-neutral-800 rounded-2xl'>
             <img className={"bg-gradient-to-r from-cyan-500 to-blue-500 object-cover rounded-2xl w-full h-64 opacity-50"} src={project.imagelink === "none" ? "./assets/images/placeholder.png" : project.imagelink} />
-            <div className={"p-5"}>
+            <div className={"p-2 py-5"}>
                 <p className={"font-Poppins text-zinc-200 text-left pl-4 font-medium text-xl"}>{project.name}</p>
                 <p className={"font-Poppins text-zinc-400 text-left pl-4 text-md mt-1"}>{project.description}</p>
                 <p className={"text-green-500 font-Grotesk pt-4 font-thin text-lg"}>{project.status}</p>
